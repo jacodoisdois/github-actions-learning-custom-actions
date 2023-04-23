d064e91ab278d96e2460d0816052054a8e8337eb
Jacó Magalhães <jacomagalhaes@gmail.com>
2023-04-23 03:38:53 -0300

    Custom JS action

---
+++ .github/actions/deploy-s3-js/index.js
@@ -0,0 +1,11 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec');
+
+function run (){
+
+  core.notice('Running deploy-s3-js action...');
+
+}
+
+run();
